3230f30eefdb71aa935bc9df9575e93a3bf6beb5
ZeDongW <878201217@qq.com>
2019-01-25 08:33:10 +0800

    Initial commit

---
+++ src/com/wzd/filedemo/FileDemo.java
@@ -0,0 +1,26 @@
+package com.wzd.filedemo;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * @author ：ZeDongW
+ * @version : Java File类示例
+ * @ClassName : FileDemo
+ * @description：1.0
+ * @modified By：
+ * @date ：Created in 2019/01/25/0025 6:25
+ */
+public class FileDemo {
+    public static void main(String[] args) {
+        File file = new File("");
+        try {
+            file.createNewFile();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println(file.exists());
+        System.out.println(file.getAbsolutePath());
+        System.out.println(File.separator);
+    }
+}
