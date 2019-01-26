ed8cf39d4e08f556cf72673545d926c5012c908e
ZeDongW <878201217@qq.com>
2019-01-27 06:13:11 +0800

    增加异常处理

--- src/com/wzd/copyfile/Copyfile.java
+++ src/com/wzd/copyfile/Copyfile.java
@@ -11,7 +11,7 @@ import java.io.*;
  * @date ：Created in 2019/01/26/0026 19:34
  */
 public class Copyfile {
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args){
         String sourcePath = "E:\\Delacey - Dream It Possible.mp3";
         String destPath = "src\\";
         sourcePath = "E:\\IMG_20190106_141012.jpg";
@@ -25,11 +25,11 @@ public class Copyfile {
      * @Param      : [sourcePath, destPath]
      * @return     : void
      */
-    private static void myCopyFile(String sourcePath, String destPath) throws IOException {
+    private static void myCopyFile(String sourcePath, String destPath) {
         //根据源文件路径，获取源文件对象
         File sourceFile = new File(sourcePath);
         if (!sourceFile.exists()){
-            throw new FileNotFoundException("该文件不存在");
+            throw new RuntimeException(new FileNotFoundException("该文件不存在"));
         }
 
         //获取源文件文件名
@@ -41,12 +41,9 @@ public class Copyfile {
         //根据目标文件路径和源文件扩展名创建目标文件
         File destFile = new File(destPath + "newFile" + expandedName);
 
+        FileInputStream fileInputStream = null;
 
-        //打开源文件的输入流
-        FileInputStream fileInputStream = new FileInputStream(sourceFile);
-
-        //打开目标文件的输出流
-        FileOutputStream fileOutputStream = new FileOutputStream(destFile);
+        FileOutputStream fileOutputStream = null;
 
         //定义一个字节数组，做读写文件的缓存数组
         byte[] buf = new byte[1024*8];
@@ -54,14 +51,33 @@ public class Copyfile {
         //字节数组的长度
         int length = 0;
 
-        //读取源文件，同时写入目标文件
-        while ((length = fileInputStream.read(buf)) != -1) {
-            fileOutputStream.write(buf, 0, length);
-        }
+        try {
+            //打开源文件的输入流
+            fileInputStream = new FileInputStream(sourceFile);
 
-        //关闭输入输出流,先开后关
-        fileOutputStream.close();
-        fileInputStream.close();
+            //打开目标文件的输出流
+            fileOutputStream = new FileOutputStream(destFile);
+
+
+            //读取源文件，同时写入目标文件
+            while ((length = fileInputStream.read(buf)) != -1) {
+                fileOutputStream.write(buf, 0, length);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } finally {
+            try {
+                //关闭输入输出流,先开后关
+                if (fileOutputStream != null){
+                    fileOutputStream.close();
+                }
+                if (fileInputStream != null){
+                    fileInputStream.close();
+                }
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 
 }
